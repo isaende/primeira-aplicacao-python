cedb1eee406d50d18229f59b4b7b8be57af37770
Isabella Monteiro <isa.ende07@gmail.com>
2025-09-17 10:44:55 -0300

    primeiro commit

---
+++ exercicios/verifica.py
@@ -0,0 +1,10 @@
+usuario = input("Digite o usuário: ")
+
+senha = int(input("Digite a senha: "))
+
+while usuario != 'Frederico':
+    usuario = input("Digite o usuário corretamente: ")
+
+
+while senha != 12345:
+    senha = int(input('Digite a senha corretamente: '))
